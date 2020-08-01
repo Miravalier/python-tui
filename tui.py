20de05b3c541339805c9a1c15504502a8dd9ff5f
Benjamin Miramontes <benjamin@miravalier.net>
2020-07-31 23:06:28 -0400

    Implemented message and prompt rendering

--- tui.py
+++ tui.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.8
+import math
 import terminal
 import shutil
 import sys
@@ -8,10 +9,13 @@ from utils import printf
 
 
 class Application:
-    def __init__(self, *, message_history=1024, command_history=1024, sgr_enabled=True):
-        self._sgr_enabled = sgr_enabled
+    translation_table = str.maketrans('\n\t', '  ')
+
+    def __init__(self, *, message_history=1024, command_history=1024, prompt=' > '):
         self._messages = deque(maxlen=message_history)
         self._commands = deque(maxlen=command_history)
+        self.prompt = prompt
+        self.update_dimensions()
 
     ##################
     # Public Methods #
@@ -20,20 +24,33 @@ class Application:
     def clear(self):
         self._messages.clear()
 
-    def write(self, *message):
-        for part in message:
-            if isinstance(part, str):
-                self._add_str(part)
-            else:
-                self._add_str(*part)
+    def print(self, *parts, sep=' ', end=''):
+        # Construct the separator and end string
+        sep = self.construct_part(sep)
+        end = self.construct_part(end)
+        # Assemble the message
+        message = []
+        for i in range(len(parts)-1):
+            message.append(self.construct_part(parts[i]))
+            message.append(sep)
+        if parts:
+            message.append(self.construct_part(parts[-1]))
+        message.append(end)
+        # Append the message to the deque
+        self._messages.appendleft(message)
+        # Re-render all messages
+        self.update_dimensions()
+        self._render_messages()
 
     def run(self):
-        with terminal.raw_mode():
-            self._render_messages()
-            self._render_prompt()
-            while True:
-                character = sys.stdin.read(1)
-                printf(repr(character))
+        try:
+            with terminal.raw_mode():
+                while True:
+                    self._render_prompt()
+                    character = sys.stdin.read(1)
+                    self.print(repr(character))
+        except (KeyboardInterrupt, EOFError):
+            pass
 
     ####################
     # Abstract Methods #
@@ -49,15 +66,50 @@ class Application:
     # Internal Methods #
     ####################
 
-    def _add_str(self, text, attributes=None):
-        self._messages.appendleft((text, attributes if self._sgr_enabled else None))
+    def row_length(self, message):
+        return math.ceil(sum(len(part) for part, attributes in message) / self.columns)
+
+    def construct_part(self, part):
+        part = part.translate(self.translation_table)
+        if isinstance(part, str):
+            return (part, None)
+        else:
+            return part
+
+    def update_dimensions(self):
+        columns, rows = shutil.get_terminal_size()
+        self.columns = columns
+        self.rows = rows
 
     def _render_messages(self):
-        columns, rows = shutil.get_terminal_size()
-        print("Cols:", columns, "Rows:", rows)
+        # Hide cursor
+        printf(HIDE_CURSOR)
+        # Start cursor on the prompt row
+        current_row = self.rows
+        for message in self._messages:
+            # Calculate how many rows will be needed for this message
+            current_row -= self.row_length(message)
+            # Stop once we reach the top of the terminal
+            if current_row < 1:
+                break
+            # Print the message into the terminal
+            printf(POSITION_CURSOR(current_row, 1))
+            columns_written = 0
+            for part, attributes in message:
+                if not part:
+                    continue
+                if attributes:
+                    printf(attributes, part, SGR_RESET)
+                else:
+                    printf(part)
+                columns_written += len(part)
+            printf(' ' * (columns_written % self.columns))
+        # Show Cursor
+        printf(SHOW_CURSOR)
 
     def _render_prompt(self):
-        pass
+        # Move cursor to the prompt row, display prompt
+        printf(POSITION_CURSOR(self.rows, 1), self.prompt)
 
 
 if __name__ == '__main__':
